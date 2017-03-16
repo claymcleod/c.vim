c0eaa567faf1e70cb747053c52a2031eccc01b5f
Clay McLeod <clay.mcleod@stjude.org>
2017-03-16 08:57:18 -0500

    Updated with my style rules.

--- c-support/codesnippets/main.cc
+++ c-support/codesnippets/main.cc
@@ -3,16 +3,13 @@
 #include <iomanip>   
 #include <iostream>  
 
-using namespace std;
-
 // ===  FUNCTION  ======================================================================
 //         Name:  main
 //  Description:  main function
 // =====================================================================================
-	int
-main ( int argc, char *argv[] )
+int main ( int argc, char *argv[] )
 {
-	cout	<< "\nProgram " << argv[0] << endl << endl;
+    std::cout << "\nProgram " << argv[0] << std::endl << std::endl;
 
 	return EXIT_SUCCESS;
 }		// ----------  end of function main  ---------- 
